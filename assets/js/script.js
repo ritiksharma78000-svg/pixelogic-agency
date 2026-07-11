795f259b22e6115ea58da9bebe9e86546b5b3f0a
Ritik <youremail@gmail.com>
2026-07-11 10:57:06 +0530

    Added Google Analytics

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("✅ All Modules Loaded");
 
 });
+window.addEventListener("scroll",()=>{
+
+    const header=document.querySelector(".header");
+
+    if(window.scrollY>50){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
